17248fdec4e90aea1ab79c5d098a1933b2309200
Ivan Garcia <icsgarcia.student@ua.edu.ph>
2025-04-15 16:02:44 +0800

    change cors settings

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ const app: Express = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "https://spoonfed-jr3xykhfd-ivancsgarcias-projects.vercel.app/",
+        origin: "https://spoonfed.vercel.app/",
         credentials: true,
     })
 );
